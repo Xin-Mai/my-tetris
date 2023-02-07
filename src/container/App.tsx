b41c5128cb8a454f783e6f96f5c30789614ddd5e
Xin-Mai <xin_mai@foxmail.com>
2023-02-07 19:17:30 +0800

    feat: init

---
+++ src/container/App.tsx
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  // the size of the page
+  const [w, setW] = useState<number>(0);
+  const [h, setH] = useState<number>(0);
+
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
